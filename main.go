374c4dd5f6d5b2b2f525104437fbfabce3b9a55e
wazave <jelizald@compusoluciones.com>
2016-11-01 07:02:18 -0600

    m

--- main.go
+++ main.go
@@ -1,25 +1,52 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"time"
 
-	"database/sql"
+	"gopkg.in/mgo.v2"
+
 	"encoding/json"
 
 	"github.com/gorilla/context"
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
+	"gopkg.in/mgo.v2/bson"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome!")
+type Tea struct {
+	Id       bson.ObjectId `json:"id,omitempty" bson:"_id,omitempty"`
+	Name     string        `json:"name"`
+	Category string        `json:"category"`
 }
 
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "you are on the about page")
+type TeaResource struct {
+	Data Tea `json:"data"`
+}
+
+type TeaRepo struct {
+	coll *mgo.Collection
+}
+
+func (r *TeaRepo) Find(id string) (TeaResource, error) {
+	result := TeaResource{}
+	err := r.coll.FindId(bson.ObjectIdHex(id)).One(&result.Data)
+	if err != nil {
+		return result, err
+	}
+	return result, nil
+}
+
+func (c *appContext) teaHandler(w http.ResponseWriter, r *http.Request) {
+	params := context.Get(r, "params").(httprouter.Params)
+	repo := TeaRepo{c.db.C("teas")}
+	tea, err := repo.Find(params.ByName("id"))
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", "application/vnd.api+json")
+	json.NewEncoder(w).Encode(tea)
 }
 
 func loggingHandler(next http.Handler) http.Handler {
@@ -50,13 +77,7 @@ type router struct {
 }
 
 type appContext struct {
-	db *sql.DB
-}
-
-func (c *appContext) teaHandler(w http.ResponseWriter, r *http.Request) {
-	params := context.Get(r, "params").(httprouter.Params)
-	log.Println(params.ByName("id"))
-	json.NewEncoder(w).Encode(nil)
+	db *mgo.Database
 }
 
 func newRouter() *router {
@@ -75,130 +96,16 @@ func (r *router) get(path string, handler http.Handler) {
 }
 
 func main() {
-	appC := appContext{nil}
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+
+	appC := appContext{session.DB("test")}
 	commonHandlers := alice.New(context.ClearHandler, loggingHandler, recoverHandler)
 	router := newRouter()
-	router.get("/about", commonHandlers.ThenFunc(aboutHandler))
-	router.get("/", commonHandlers.ThenFunc(indexHandler))
 	router.get("/teas/:id", commonHandlers.ThenFunc(appC.teaHandler))
 	http.ListenAndServe(":8080", router)
 }
-
-// package main
-
-// import (
-// 	"database/sql"
-// 	"encoding/json"
-// 	"errors"
-// 	"fmt"
-// 	"log"
-// 	"net/http"
-// 	"time"
-
-// 	"github.com/gorilla/context"
-// 	"github.com/julienschmidt/httprouter"
-// 	"github.com/justinas/alice"
-// )
-
-// func recoverHandler(next http.Handler) http.Handler {
-// 	fn := func(w http.ResponseWriter, r *http.Request) {
-// 		defer func() {
-// 			if err := recover(); err != nil {
-// 				log.Printf("panic: %+v", err)
-// 				http.Error(w, http.StatusText(500), 500)
-// 			}
-// 		}()
-
-// 		next.ServeHTTP(w, r)
-// 	}
-
-// 	return http.HandlerFunc(fn)
-// }
-
-// func loggingHandler(next http.Handler) http.Handler {
-// 	fn := func(w http.ResponseWriter, r *http.Request) {
-// 		t1 := time.Now()
-// 		next.ServeHTTP(w, r)
-// 		t2 := time.Now()
-// 		log.Printf("[%s] %q %v\n", r.Method, r.URL.String(), t2.Sub(t1))
-// 	}
-
-// 	return http.HandlerFunc(fn)
-// }
-
-// func aboutHandler(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintf(w, "You are on the about page.")
-// }
-
-// func indexHandler(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintf(w, "Welcome!")
-// }
-
-// type appContext struct {
-// 	db *sql.DB
-// }
-
-// func (c *appContext) authHandler(next http.Handler) http.Handler {
-// 	fn := func(w http.ResponseWriter, r *http.Request) {
-// 		authToken := r.Header.Get("Authorization")
-// 		user, err := map[string]interface{}{}, errors.New("test")
-// 		// user, err := getUser(c.db, authToken)
-// 		log.Println(authToken)
-
-// 		if err != nil {
-// 			http.Error(w, http.StatusText(401), 401)
-// 			return
-// 		}
-
-// 		context.Set(r, "user", user)
-// 		next.ServeHTTP(w, r)
-// 	}
-
-// 	return http.HandlerFunc(fn)
-// }
-
-// func (c *appContext) adminHandler(w http.ResponseWriter, r *http.Request) {
-// 	user := context.Get(r, "user")
-// 	// Maybe other operations on the database
-// 	json.NewEncoder(w).Encode(user)
-// }
-
-// func (c *appContext) teaHandler(w http.ResponseWriter, r *http.Request) {
-// 	params := context.Get(r, "params").(httprouter.Params)
-// 	log.Println(params.ByName("id"))
-// 	// tea := getTea(c.db, params.ByName("id"))
-// 	json.NewEncoder(w).Encode(nil)
-// }
-
-// // We could also put *httprouter.Router in a field to not get access to the original methods (GET, POST, etc. in uppercase)
-// type router struct {
-// 	*httprouter.Router
-// }
-
-// func (r *router) Get(path string, handler http.Handler) {
-//     log.Println(path)
-// 	r.GET(path, wrapHandler(handler))
-// }
-
-// func NewRouter() *router {
-// 	return &router{httprouter.New()}
-// }
-
-// func wrapHandler(h http.Handler) httprouter.Handle {
-// 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-// 		context.Set(r, "params", ps)
-// 		h.ServeHTTP(w, r)
-// 	}
-// }
-
-// func main() {
-// 	// db := sql.Open("postgres", "...")
-// 	appC := appContext{nil}
-// 	commonHandlers := alice.New(context.ClearHandler, loggingHandler, recoverHandler)
-// 	router := NewRouter()
-// 	router.Get("/admin", commonHandlers.Append(appC.authHandler).ThenFunc(appC.adminHandler))
-// 	router.Get("/about", commonHandlers.ThenFunc(aboutHandler))
-// 	router.Get("/", commonHandlers.ThenFunc(indexHandler))
-// 	router.Get("/teas/:id", commonHandlers.ThenFunc(appC.teaHandler))
-// 	http.ListenAndServe(":8080", router)
-// }
